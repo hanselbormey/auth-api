734c06d5ca92023d46600ba22db94c8e62bf1096
Hansel Bormey <hanselbormey@gmail.com>
2023-03-23 13:12:41 -0400

    Remove api_host environment variable

--- server.ts
+++ server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import * as dotenv from 'dotenv';
 import users from './routes/users';
 
 const app = express();
-dotenv.config();
 app.use(bodyParser.json());
-app.use(`${process.env.API_HOST}/users`, users);
+app.use('/api/users', users);
 
 app.listen(3001);
