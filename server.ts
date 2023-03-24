3d332b46c0a0ddc1ba6d03f0503fe40021ba5ee5
Hansel Bormey <hanselbormey@gmail.com>
2023-03-24 17:11:48 -0400

    Add cors dependency

--- server.ts
+++ server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+
 import users from './routes/users';
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use('/api/users', users);
 
 app.listen(3001);
